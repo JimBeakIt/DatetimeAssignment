a35826099e98d0570df9a771fde7224ec221b9ba
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-12-12 10:26:34 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            
+
+            Console.WriteLine("Enter a number:");
+            int enterNum = Convert.ToInt32(Console.ReadLine());
+
+            DateTime now2 = now.AddHours(enterNum);
+            Console.WriteLine("Updated time is: " + now2.ToString());
+            Console.ReadLine();
+        }
+    }
+}
